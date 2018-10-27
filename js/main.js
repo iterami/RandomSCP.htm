c094b7cff0fb69aa0f7e8d245798bd26da8bb264
Jan Hořava <czechinthemail@gmail.com>
2018-10-27 13:34:30 -0700

    changed 1000 to 999 in display and generate()

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ function repo_init(){
     let max = 5000;
     let buttons = '';
     for(let i = 0; i < max; i+= 1000){
-        buttons += '<tr><td><input id=generate-' + i + ' type=button value="Between ' + i + ' and ' + (i + 1000) + '">';
+        buttons += '<tr><td><input id=generate-' + i + ' type=button value="Between ' + i + ' and ' + (i + 999) + '">';
     }
     document.getElementById('buttons').innerHTML += buttons;
     for(let i = 0; i < max; i+= 1000){
         document.getElementById('generate-' + i).onclick = function(){
             generate({
-              'max': i + 1000,
+              'max': i + 999,
               'min': i,
             });
         };
