baa3eadfb004d5aff3107fbac6f4b664d86739b7
Jan Hořava <czechinthemail@gmail.com>
2018-10-27 13:30:17 -0700

    moved core_repo_init() call after button creation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
 'use strict';
 
 function repo_init(){
-    core_repo_init({
-      'events': {
-        'generate': {
-          'onclick': generate,
-        },
-      },
-      'keybinds': {
-        13: {
-          'todo': generate,
-        },
-      },
-      'title': 'RandomSCP.htm',
-    });
-
     let max = 5000;
     let buttons = '';
     for(let i = 0; i < max; i+= 1000){
@@ -29,4 +15,18 @@ function repo_init(){
             });
         };
     }
+
+    core_repo_init({
+      'events': {
+        'generate': {
+          'onclick': generate,
+        },
+      },
+      'keybinds': {
+        13: {
+          'todo': generate,
+        },
+      },
+      'title': 'RandomSCP.htm',
+    });
 }
